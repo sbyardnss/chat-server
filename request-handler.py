1d46ba1109f1cd1fe5b17e62b31cf8708f343cc3
Mary Williams <sbyardnss@gmail.com>
2023-05-17 14:29:48 -0500

    initial with working responses after post request

---
+++ request-handler.py
@@ -0,0 +1,70 @@
+import json
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse
+import os
+import openai
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+# Here's a class. It inherits from another class.
+# For now, think of a class as a container for functions that
+# work together for a common purpose. In this case, that
+# common purpose is to respond to HTTP requests from a client.
+class HandleRequests(BaseHTTPRequestHandler):
+    # This is a Docstring it should be at the beginning of all classes and functions
+    # It gives a description of the class or function
+    """Controls the functionality of any GET, PUT, POST, DELETE requests to the server
+    """
+    
+    # Here's a class function
+    
+
+    # Here's a method on the class that overrides the parent's method.
+    # It handles any POST request.
+    def do_POST(self):
+        """Handles POST requests to the server"""
+        # client will send a conversation array to server
+        # send to openai api and get response
+        # return response to client
+        self._set_headers(201)
+        if self.path == "/chat":
+            content_len = int(self.headers.get('content-length', 0))
+            post_body = self.rfile.read(content_len)
+            
+            messages = json.loads(post_body)
+            chat_completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+            print(chat_completion.choices[0].message.content)            
+            self.wfile.write(json.dumps(chat_completion).encode())
+
+    def _set_headers(self, status):
+        """Sets the status code, Content-Type and Access-Control-Allow-Origin
+        headers on the response
+
+        Args:
+            status (number): the status code to return to the front end
+        """
+        self.send_response(status)
+        self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.end_headers()
+
+    def do_OPTIONS(self):
+        """Sets the options headers
+        """
+        self.send_response(200)
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE')
+        self.send_header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept')
+        self.end_headers()
+
+
+def main():
+    """Starts the server on port 8088 using the HandleRequests class
+    """
+    host = ''
+    port = 8088
+    HTTPServer((host, port), HandleRequests).serve_forever()
+
+
+if __name__ == "__main__":
+    main()
